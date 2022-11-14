646302105abff72b02a7b07e3c340199731ed226
Xaria A <xaria.shella@gmail.com>
2022-11-14 09:55:51 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+       /* boolean x = true;
+        boolean y = false;
+        System.out.print(x&&y);
+        System.out.print(" ");
+        System.out.print(x||y);
+        System.out.print(" ");
+        System.out.print(! (x&&y));
+        System.out.print(" ");
+        System.out.println(! (x||y));
+        System.out.print(" ");*/
+
+        System.out.println(" ");
+
+        int x=5;
+        System.out.print(x++);
+        System.out.print(" ");
+        int x = 10
+        System.out.print(++x);
+        
+        }
+}
