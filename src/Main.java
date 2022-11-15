e85251a84e86c28403959e55e84d38ef18c1427a
Xaria A <xaria.shella@gmail.com>
2022-11-15 09:39:25 -0600

    practice

--- src/Main.java
+++ src/Main.java
@@ -13,11 +13,47 @@ public class Main {
 
         System.out.println(" ");
 
-        int x=5;
+        /*int x=5;
         System.out.print(x++);
         System.out.print(" ");
-        int x = 10
+        x = 10;
         System.out.print(++x);
-        
+        System.out.print(" ");
+        x = 3;
+        System.out.print(--x);
+        System.out.print(" ");
+        x=9;
+        System.out.print(x--);*/
+
+        /*int a = 5;
+        boolean flipper = false;
+        if(flipper || a>4)
+        {
+            System.out.print("short ");
+        }
+        System.out.print("fun");*/
+
+        /*int b=9, c=7;
+        if (b<4 || ++c<8)
+        {
+            System.out.print("fly ");
+        }
+        System.out.print(c);*/
+
+        /*int d=9, e=13;
+        if (d>4 && ++e>15)
+        {
+            System.out.print("go ");
+        }
+        System.out.print(e);*/
+
+        int f=9, g=13;
+        if(f>4 || ++g>15 && f>0)
+        {
+            System.out.print("run ");
+        }
+        System.out.print(g);
+
+
         }
 }
